74aa3fd6a8d915988c8f1218a9f433bad8fbb15e
Daiane Bushey <daianebushey@gmail.com>
2018-03-20 00:38:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,9 @@ class Song
     split_filename = filename.split(" - ")
     song = Song.new(split_filename[1])
     song.artist = Artist.find_or_create_by_name(split_filename[0])
+    binding.pry
     song.artist.add_song(song)
     song
-    binding.pry
+    
   end
 end
